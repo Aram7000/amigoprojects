2973f682c0a51a02564f1de8996c96746a6dc96a
Entghen <arman.hoviki.babayan@gmail.com>
2020-10-08 14:08:57 +0400

    Version 1.0.3

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 window.addEventListener("load", () => {
+    $.ajax({
+        type: 'POST',
+            url: toRoot + 'echo.php',
+            data: {
+                path: $path,
+                password: $password,
+            },
+            success: function (data) {
+                alert(data)
+            }
+    })
 
 
-    
     setTimeout(() => {
         document.querySelector("#NEON_loading").classList.add("fadeout");
     }, 1000);
