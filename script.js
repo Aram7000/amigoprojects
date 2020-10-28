b3a1e3008fa77152c9227e004753e6cb1348f1b9
Aram7000 <aram.khachatryan.7000@gmail.com>
2020-10-28 12:57:07 +0400

    Menu

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 window.addEventListener("load", () => {
-    $.ajax({
-        type: 'POST',
-            url: toRoot + 'echo.php',
-            data: {
-                data: "asd",
-            },
-            success: function (data) {
-                alert(data)
-            }
-    })
 
+
+    n_onload();
 
     setTimeout(() => {
         document.querySelector("#NEON_loading").classList.add("fadeout");
