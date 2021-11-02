308cf2549caab5b98ac13e05f12e03c6d4ae5cbc
Aram7000 <61659984+Aram7000@users.noreply.github.com>
2021-11-02 11:00:45 +0300

    Update index.js

--- Jor/index.js
+++ Jor/index.js
@@ -1,4 +1,5 @@
 setup = () => {
-  
+  let scn = scene(0, 0, window.innerWidth, window.innerHeight);
+  let person = cube(0, 0, 0, 100, 100, 100, scn, "linear-gradient(45deg, #ff0000, #553300)");
   
 }
