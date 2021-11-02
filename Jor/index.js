80e3b688034000ab6fbc22c7b8701ee6e4a166d6
Aram7000 <61659984+Aram7000@users.noreply.github.com>
2021-11-02 11:04:32 +0300

    Update index.js

--- Jor/index.js
+++ Jor/index.js
@@ -2,4 +2,13 @@ setup = () => {
   let scn = scene(0, 0, window.innerWidth, window.innerHeight);
   let person = cube(0, 0, 0, 100, 100, 100, scn, "radial-gradient(#ff0000, #553300)");
   
+  let y = 0;
+  
+  
+  document.addEventListener("keydown", () => {
+    y += 10;
+    person.cube.style.top = y + "px";
+    
+  });
+  
 }
